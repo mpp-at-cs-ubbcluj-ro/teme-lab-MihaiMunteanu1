c93df9634d55cf31e62507267c593249e0b8efa8
mihai <mihai.munteanu@stud.ubbcluj.ro>
2025-03-03 18:45:27 +0200

    Test commit

--- module/src/Main.java
+++ module/src/Main.java
@@ -1,3 +1,5 @@
+import domain2.Casier2;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
