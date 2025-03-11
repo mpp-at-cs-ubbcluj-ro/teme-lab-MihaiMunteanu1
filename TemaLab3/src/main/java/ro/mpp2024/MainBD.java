cad70dcfbec76a6000f87da11a1928fe1dbce0f8
Mihai Munteanu <mihai.munteanu@stud.ubbcluj.ro>
2025-03-11 19:17:25 +0200

    Commit temaLab3

--- TemaLab3/src/main/java/ro/mpp2024/MainBD.java
+++ TemaLab3/src/main/java/ro/mpp2024/MainBD.java
@@ -2,6 +2,7 @@ package ro.mpp2024;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
 
 public class MainBD {
@@ -31,7 +32,7 @@ public class MainBD {
             System.out.println(car);
 
         System.out.println("Modificam o masina:");
-        Car car = new Car("Tesla","Model S", 1010);
+        Car car = new Car("Tesla","Model S", 10);
         carRepo.update(4,car);
         System.out.println("Toate masinile din db");
         for(Car car1:carRepo.findAll())
@@ -40,6 +41,14 @@ public class MainBD {
         System.out.println("Cautam masini din ultimii 10 ani");
         for(Car car2:carRepo.findBetweenYears(2015,2025)){
             System.out.println(car2);
+        }
+
+        System.out.println("Afisare dupa o conditie: sa fie SEAT");
+        for(Car car3:carRepo.findBetweenYears(2015,2025)){
+            if(Objects.equals(car3.getManufacturer(), "Seat"))
+            {
+                System.out.println(car3);
+            }
         }
     }
 }
